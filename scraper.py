97a46517e41d56eaa9022852b1d3628056f6e019
Ra'ad Aldakhil <aldakhil95@gmail.com>
2021-08-13 00:17:01 +0300

    fix saved post index

--- scraper.py
+++ scraper.py
@@ -8,4 +8,4 @@ print(user.biography)
 posts = user.posts
 for index, post in enumerate(posts):
     r = requests.get(post[8],allow_redirects=True)
-    open('post#'+str(index),'wb').write(r.content)
+    open('post#'+str(index+1),'wb').write(r.content)
